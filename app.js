6a6f3729879e6d0747e617515754c9c9a9f24c07
alan840618 <kb3789@gmail.com>
2023-03-30 00:09:04 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const app = express()
+mongoose.connect(process.env.MONGODB_URI)
+const db = mongoose.connection
+db.on('error',()=>{
+  console.log('mongodb error!')
+})
+db.once('open',()=>{
+  console.log('mongodb connected!')
+})
 app.get('/',(req,res)=>{
   res.send('hello world')
 })
