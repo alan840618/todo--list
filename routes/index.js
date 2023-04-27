d126497e771a6de6bade29cdb7266569f8eaad47
alan840618 <kb3789@gmail.com>
2023-04-27 20:29:53 +0800

    feat: add user module

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const router = express.Router()
 //準備引入路由模組
 const home = require('./modules/home')
 const todos = require('./modules/todos')
+const users = require('./modules/users')
 router.use('/',home)
 router.use('/todos',todos)
+router.use('/users',users)
 //匯出路由器
 module.exports = router
